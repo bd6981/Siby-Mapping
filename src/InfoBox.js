4606be0c3576ac89b89eb1f0895091afd46d1607
bd6981 <brittanyjdavis22@gmail.com>
2023-02-03 03:04:06 -0500

    corrected old siby

---
+++ src/InfoBox.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./map.css";
+
+const InfoBox = ({ info }) => {
+  return (
+    <>
+      <div className="infoBox">
+        <header>
+          <h2>Crime Info</h2>
+        </header>
+
+        <div>
+          <p>
+            CRIME: <strong>{info.title}</strong>
+          </p>
+          <p>
+            ID: <strong>{info.id}</strong>
+          </p>
+          <p>
+            DATE: <strong>{info.date}</strong>
+          </p>
+          <p>
+            OCCUR ON A: <strong>{info.day}</strong>
+          </p>
+          <p>
+            TIME: <strong>{info.time}</strong>
+          </p>
+          <p>
+            ON SCENE DATE: <strong>{info.osdate}</strong>
+          </p>
+          <p>
+            ON SCENE TIME: <strong>{info.ostime}</strong>
+          </p>
+          <p>
+            BEAT: <strong>{info.beat}</strong>
+          </p>
+          <p>
+            ZONE: <strong>{info.zone}</strong>
+          </p>
+          <p>
+            LOCATION: <strong>{info.location}</strong>
+          </p>
+          <p>
+            IBR CODE: <strong>{info.ibr}</strong>
+          </p>
+          <p>
+            NEIGHBORHOOD: <strong>{info.neighborhood}</strong>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default InfoBox;
