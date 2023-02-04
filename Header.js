edcdfd9ad35df67c5b4cdbbe1a993493338e58e8
bd6981 <brittanyjdavis22@gmail.com>
2023-02-04 13:10:33 -0500

    added header content

--- Header.js
+++ Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import './Header.css'
+import myImage from '../Auth/img1.png'
+import {
+
+ 
+  Image,
+
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <header>
+        <Image
+          src={myImage}
+          style={{ height: "15vh", width: "15vw", textAlign: "center" }}
+        />
+      </header>
     </div>
-  )
+  );
 }
-
 export default Header
